7e991f67ad54774f505fa30b382af6578e04e220
Ed Penington <edpenington@Eds-MacBook-Air.local>
2024-11-05 16:04:00 +0000

    Update to simple simmer waiting time simulator

--- model/run_model.R
+++ model/run_model.R
@@ -2,20 +2,74 @@
 # TITLE: run_model.R
 # DESCRIPTION: Run economic model of psychosis and store results
 # AUTHOR: Ed Penington
-# DATE: 2024/02/19
-# VERSION: 0.1
+# DATE: 2024/11/05
+# VERSION: 0.1.1
 
+# Clear the workspace
+rm(list = ls())
+
+# Load packages
 library(simmer)
-
-simenv <- simmer("EconomicModelPsychosis")
+library(simmer.plot)
+library(tidyverse)
 
 # PARAMETERS --------------------------------------------------------------
 
-# EVENT STRUCTURE ---------------------------------------------------------
+# EIP duration
+fun_EIPduration <- function() {
+  
+  t_EIPduration <- rnorm(n=1, mean = 3*365.25, sd = 50)
+  
+  return(t_EIPduration)
+}
 
-# EXECUTE MODEL -----------------------------------------------------------
+# TRAJECTORIES ------------------------------------------------------------
+traj <- trajectory(name = "FEP") %>%
+  # Starting point: referral for suspected FEP
 
-simenv
+  # Enter queue for EIP
+  seize(resource = "coordinator", amount = 1) %>%
 
-# MODEL OUTPUT ------------------------------------------------------------
+  # Calculate time in EIP
+  set_attribute(keys = "t_EIPduration", values = function() fun_EIPduration()) %>%
+  timeout_from_attribute(key = "t_EIPduration") %>%
+  
+  # Discharge
+  release(resource = "coordinator", amount = 1)
+
+plot(traj, verbose=TRUE)
+
+# SIMULATION --------------------------------------------------------------
+
+# Set simulation parameters
+n_serviceusers <- 500
+t_serviceusergap <- 2
+
+# Create main simulation environment
+sim <- simmer(name = "sim") %>%
+  add_resource(name = "coordinator", capacity = 112, mon = 2) %>%
+  add_generator(name_prefix = "serviceuser",
+                trajectory = traj,
+                distribution = at(seq(from = 1, to = n_serviceusers * t_serviceusergap,
+                                      by = t_serviceusergap)),
+                mon = 2)
+  
+
+# Run simulations
+random_seed <- 123
+set.seed(random_seed)
+sim %>% reset() %>% run(until = n_serviceusers * t_serviceusergap + (365.25 * 3) )
+
+df_sim <- get_mon_attributes(.env = sim)
+df_resources <- get_mon_resources(.env = sim)
+df_arrivals <- get_mon_arrivals(.env = sim, per_resource = TRUE, ongoing = TRUE)
+
+# VALIDATION --------------------------------------------------------------
+
+# Caseload
+## TO DO: Calculate caseload for each time point
+
+# Waiting time
+## TO DO: Calculate waiting time at each time point
+
 
